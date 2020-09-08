0021990a7d1398358bc3a94a0d0b67dcdb2a6eeb
eseunghwan <reignstarbits@gmail.com>
2020-09-09 01:32:29 +0900

    first commit

--- build.py
+++ build.py
@@ -4,9 +4,19 @@ import os, sys, shutil
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 os.chdir(__dir__)
 
+# get project name
 project_name = os.path.basename(__dir__)
+
+# set dist dir
 dist_dir = os.path.join(__dir__, "build", project_name)
 
+# make dist of frontend
+if os.path.exists(os.path.join(__dir__, "frontend", "package.json")):
+    os.chdir(os.path.join(__dir__, "frontend"))
+    os.system("npm run build")
+    os.chdir(__dir__)
+
+# make dist of backend
 os.system(" ".join([
     sys.executable, "-m", "PyInstaller", "./start.py",
     "--workpath=./build/temp", "--distpath=./build",
@@ -14,9 +24,12 @@ os.system(" ".join([
     "--onedir", "--exclude-module=matplotlib", "--noconfirm", "--clean"
 ]))
 
+# create frontend and copy dist
 os.mkdir(os.path.join(dist_dir, "frontend"))
 shutil.copytree(
     os.path.join(__dir__, "frontend", "dist"),
     os.path.join(dist_dir, "frontend", "dist")
 )
+
+# remove temp
 shutil.rmtree(os.path.join(__dir__, "build", "temp"))
--- start.py
+++ start.py
@@ -3,15 +3,12 @@ import os
 from backend.utils import read_config
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
-cur_cwd = os.getcwd()
 
 # make dist of frontend
 if os.path.exists(os.path.join(__dir__, "frontend", "package.json")):
     os.chdir(os.path.join(__dir__, "frontend"))
     os.system("npm run build")
-    os.chdir(cur_cwd)
-
-del cur_cwd
+    os.chdir(__dir__)
 
 # run server
 from backend.server import start_server
