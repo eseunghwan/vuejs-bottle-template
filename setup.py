b60bf64ad7302c73bb237911daefcd2146e8c516
eseunghwan <reignstarbits@gmail.com>
2020-09-09 09:48:57 +0900

    initial commit

--- setup.py
+++ setup.py
@@ -5,9 +5,15 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 os.chdir(__dir__)
 
 if not os.path.exists(os.path.join(__dir__, "env")):
-    os.system(f"{sys.executable} -m pip install virtualenv")
-    os.system(f"{sys.executable} -m virtualenv ./env")
-    os.system(f"{os.path.join(__dir__, 'env', 'Scripts', 'python')} -m pip install -r ./requirements.txt")
+    if sys.platform.startswith("linux"):
+        # in case of linux
+        os.system(f"{sys.executable} -m venv ./env")
+        os.system(f"{os.path.join(__dir__, 'env', 'bin', 'python')} -m pip install -r ./requirements.txt")
+    elif sys.platform.startswith("win"):
+        # in case of windows
+        os.system(f"{sys.executable} -m pip install virtualenv")
+        os.system(f"{sys.executable} -m virtualenv ./env")
+        os.system(f"{os.path.join(__dir__, 'env', 'Scripts', 'python')} -m pip install -r ./requirements.txt")
 
 os.chdir(os.path.join(__dir__, "frontend"))
 os.system("npm install")
