56c6888aab6f32885fe8d73deb77fd8eccdedbff
eseunghwan <reignstarbits@gmail.com>
2020-09-14 18:49:16 +0900

    template update(20200914)

--- backend/fn/__init__.py
+++ backend/fn/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .index import get_test, set_test
+from .Index import get_test, set_test
 
 def register(bottle_app):
     bottle_app.get("/fn/get", callback = get_test)
